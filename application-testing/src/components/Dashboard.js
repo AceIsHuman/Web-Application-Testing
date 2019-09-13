4b57b5485df3e4fc3c066720ca7708657ece2436
AceIsHuman <menoman1995@yahoo.com>
2019-09-12 22:58:04 -0500

    build dashboard component

---
+++ application-testing/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <button>Ball</button>
+      <button>Strike</button>
+      <button>Foul</button>
+    </div>
+  );
+}
+
+export default Dashboard;
